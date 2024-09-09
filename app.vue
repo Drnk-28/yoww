80ea6510ac620e06194e0406f72cf1ad89208b01
drnk.28 <fuadfuuk12345@gmail.com>
2024-09-10 05:49:05 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -12,21 +12,6 @@
          click: () => {
             console.log('Edit')
          }
-  }, {
-         label: 'Duplicate',
-         icon: 'i-heroicons-document-duplicate-20-solid',
-         shortcuts: ['D'],
-         disabled: true
-  }], [{
-         label: 'Archive',
-         icon: 'i-heroicons-archive-box-20-solid'
-  }, {
-         label: 'Move',
-         icon: 'i-heroicons-arrow-right-circle-20-solid'
-  }], [{
-         label: 'Delete',
-         icon: 'i-heroicons-trash-20-solid',
-         shortcuts: ['⌘', 'D']
   }]
 ]
    const clrs = ["blue", "violet", "green", "emerald", "lime", "red", "yellow", "indigo", "cyan", "sky", "rose", "pink"]
@@ -34,10 +19,12 @@
 </script>
 
 <template>
+   <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+      <UButton :color="random" label="DRNK" icon="i-flowbite-bars-from-left-outline" variant="soft"/>
+   </UDropdown>
+</template>
+<template>
    <UContainer>
-      <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-         <UButton :color="random" label="DRNK" icon="i-flowbite-bars-from-left-outline" />
-      </UDropdown>
       <UCard class="mt-10">
          <div>
             <h1>Hi Guys</h1>
